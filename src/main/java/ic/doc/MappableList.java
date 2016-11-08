5d3c55edf8f352ace8a8eaa6b4c0934b5324ea11
dsg115 <dsg115>
2016-11-08 17:25:06 +0000

    [DG] [MZC] Made map concurrent

--- src/main/java/ic/doc/MappableList.java
+++ src/main/java/ic/doc/MappableList.java
@@ -1,14 +1,39 @@
 package ic.doc;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 public class MappableList<T> implements Iterable<T> {
     private final List<T> list = new ArrayList<>();
 
-    public MappableList<T> map(Function<T> f) {
+    public MappableList<T> map(final Function<T> f) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        Set<Future<T>> futureSet = new LinkedHashSet<>();
+        for (final T elem : list) {
+            futureSet.add(executorService.submit(new Callable<T>() {
+                @Override
+                public T call() throws Exception {
+                    return f.applyTo(elem);
+                }
+            }));
+        }
+
+        MappableList<T> result = fillMap(futureSet);
+
+        executorService.shutdown();
+
+        return result;
+    }
+
+    private MappableList<T> fillMap(Set<Future<T>> futureSet) {
         MappableList<T> result = new MappableList<>();
-        for (Iterator<T> i = list.iterator(); i.hasNext(); ) {
-            result.insert(f.applyTo(i.next()));
+
+        for (Iterator<Future<T>> i = futureSet.iterator(); i.hasNext(); ) {
+            try {
+                result.insert(i.next().get());
+            } catch (Exception e) {
+                break;
+            }
         }
         return result;
     }
